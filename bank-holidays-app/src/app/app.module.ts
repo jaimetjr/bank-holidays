d9f83a2e75bceb79d87de2e6503ec25d73b08364
Jaime Tasca Junior <JUJ1WRN@bosch.com>
2022-02-26 00:28:13 -0300

    changes in angular project, creation of the structual project

--- bank-holidays-app/src/app/app.module.ts
+++ bank-holidays-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchGridComponent } from './components/search-grid/search-grid.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    SearchGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ bank-holidays-app/src/app/components/search-grid/search-grid.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'bh-search-grid',
+    templateUrl: './search-grid.component.html',
+    styleUrls: ['./search-grid.component.css']
+})
+export class SearchGridComponent implements OnInit {
+
+    constructor() {
+        
+    }
+
+    ngOnInit(): void {
+        
+    }
+    
+}
